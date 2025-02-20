8cc882eb753fe9f33b50cd5aac89d8386ff65c0b
Hesham Alsaqqaf <heshamalsaqqaf14@gmail.com>
2025-02-20 23:20:44 +0300

    Upload Higher Order Functions Filter Method.

--- Higher Order Functions/filter.js
+++ Higher Order Functions/filter.js
@@ -9,12 +9,11 @@ console.log(filterFriendsName);
 
 // TODO: Exm 2
 let numbers = [6, 12, 15, 20, 17, 8, 5];
-
-// ? Filter Arrow Functions
+// ? Use Filter Arrow Functions
 let filterNumbersArrow = numbers.filter((element) => element % 2 === 0);
 console.log(filterNumbersArrow);
 
-// ? Filter Arrow Functions
+// ? Use Filter Arrow Functions
 let filterNumbersAnonymous = numbers.filter(function (element) {
     return element % 2 === 0;
 });
@@ -23,19 +22,16 @@ console.log(filterNumbersAnonymous);
 
 // TODO: Exm => Practice Filter Method
 let mix = "A13BS2ZX";
-
-// ?Anonymous Functions
+// ?Use Anonymous Functions
 let filterNumberAnonymous = mix.split("").filter(function (element) {
     return parseInt(element);
 }).map(function (element) {
     return element * element;
 }).join("");
-
 console.log("Anonymous: ", filterNumberAnonymous);
 
-// ?Arrow Functions
+// ?use Arrow Functions
 let filterNumberArrow = mix.split("")
     .filter((element) => parseInt(element))
     .map((element) => element * element).join("");
-
 console.log("Arrow: ", filterNumberArrow);
