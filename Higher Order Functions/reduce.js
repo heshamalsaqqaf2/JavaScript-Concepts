add1a45e38e712a8b9877ece8656f0cdbea214da
Hesham Alsaqqaf <heshamalsaqqaf14@gmail.com>
2025-02-20 23:36:57 +0300

    Upload Higher Order Functions Reduce Method.

---
+++ Higher Order Functions/reduce.js
@@ -0,0 +1,14 @@
+'use strict';
+
+let numbers = [10, 20, 30, 40];
+
+let reduceNumber = numbers.reduce(function (accumulator, currentElement, index, array) {
+    console.log(`Accumulator ${accumulator}`);
+    console.log(`Current Element ${currentElement}`);
+    console.log(`Index ${index}`);
+    console.log(`Array ${array}`);
+    console.log("Sum Array of Numbers = ", accumulator + currentElement);
+    console.log(`----------------------------`);
+    return accumulator + currentElement;
+});
+console.log(reduceNumber);
