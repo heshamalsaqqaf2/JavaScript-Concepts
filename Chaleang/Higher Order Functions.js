93343e32f72271eecadbd16c5331b869028cc745
Hesham Alsaqqaf <heshamalsaqqaf14@gmail.com>
2025-02-21 18:32:56 +0300

    Higher Order Functions Challenges.

--- Chaleang/Higher Order Functions.js
+++ Chaleang/Higher Order Functions.js
@@ -1,20 +1,20 @@
-/*
-  Higher Order Functions Challenges
-
-  You Can Use
-  - ,
-  - _
-  - Space
-  - True => 1 => One Time Only In The Code
-
-  You Cannot Use
-  - Numbers
-  - Letters
-
-  - You Must Use [Filter + Map + Reduce + Your Knowledge]
-  - Order Is Not Important
-  - All In One Chain
-
+/**
+ * @description : Higher Order Functions Challenges
+ *  * You Can Use
+ *     ,
+ *     _
+ *     Space
+ *     True => 1 => One Time Only In The Code
+ * 
+ *  * You Cannot Use
+ *     Numbers
+ *     Letters
+ *  
+ *  ? You Must Use [Filter + Map + Reduce + Your Knowledge]
+ *  ? Order Is Not Important
+ *  ? All In One Chain
+ * 
+ * @summary : Hesham Zohir Alsaqqaf
 */
 
 let myString = "1,2,3,HH,e,s,h,a,m,_,Z,o,h,i,r,_,A,l,s,a,q,q,a,f,2,0,Z";
