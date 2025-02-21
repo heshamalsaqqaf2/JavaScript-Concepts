56c9202fec3c62b3da7599b0f7f68a8738f59b3c
Hesham Alsaqqaf <heshamalsaqqaf14@gmail.com>
2025-02-21 18:28:05 +0300

    Higher Order Functions Challenges.

---
+++ Chaleang/Higher Order Functions.js
@@ -0,0 +1,30 @@
+/*
+  Higher Order Functions Challenges
+
+  You Can Use
+  - ,
+  - _
+  - Space
+  - True => 1 => One Time Only In The Code
+
+  You Cannot Use
+  - Numbers
+  - Letters
+
+  - You Must Use [Filter + Map + Reduce + Your Knowledge]
+  - Order Is Not Important
+  - All In One Chain
+
+*/
+
+let myString = "1,2,3,HH,e,s,h,a,m,_,Z,o,h,i,r,_,A,l,s,a,q,q,a,f,2,0,Z";
+
+let solution = myString.split(",").map(function (element) {
+    return element === '_' ? ' ' : element;
+}).filter(function (element) {
+    return isNaN(parseInt(element));
+}).reduce(function (acc, element) {
+    return `${acc}${element}`;
+}).slice(true, -isNaN(myString)); // (1, -1)
+
+console.log(solution); // Hesham Zohir Alsaqqaf
